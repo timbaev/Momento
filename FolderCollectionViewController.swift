095085d45f9abf5e6081901c35793c63c1ac42ad
Elina <elina@Elinas-Mac.local>
2017-07-14 16:12:14 +0300

    Little changes

--- FolderCollectionViewController.swift
+++ FolderCollectionViewController.swift
@@ -33,15 +33,6 @@ class FolderCollectionViewController: UICollectionViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     // MARK: UICollectionViewDataSource
 
@@ -86,4 +77,5 @@ class FolderCollectionViewController: UICollectionViewController {
             let destinationVC = segue.destination as! CollectionViewController
             destinationVC.cellDataArray = folderArray[index].cellsArray
         }
-    }}
+    }
+}
