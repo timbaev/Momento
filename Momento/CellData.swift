27a7f139ccfa43637e2f7ae1b79609ec42893807
Тимур Шафигуллин <timursafigullin@Timurs-Mac.local>
2017-07-18 17:14:52 +0300

    Сделано сохранение на устройство

--- Momento/CellData.swift
+++ Momento/CellData.swift
@@ -14,3 +14,41 @@ struct CellData {
     var cellImage: UIImage
     
 }
+
+extension CellData {
+    init?(data: NSData) {
+        let coding = NSKeyedUnarchiver.unarchiveObject(with: data as Data) as! Encoding
+        cellDescription = coding.cellDescription as String
+        cellText = coding.cellText as String
+        cellImage = coding.cellImage as! UIImage
+    }
+    
+    func encode() -> NSData {
+        return NSKeyedArchiver.archivedData(withRootObject: Encoding(self)) as NSData
+    }
+    
+    private class Encoding: NSObject, NSCoding {
+        let cellDescription: NSString
+        let cellText: NSString
+        let cellImage: NSObject
+        
+        init(_ cellData: CellData) {
+            cellDescription = cellData.cellDescription as NSString
+            cellText = cellData.cellText as NSString
+            cellImage = cellData.cellImage
+        }
+        
+        @objc required init?(coder aDecoder: NSCoder) {
+            let cellDescription = aDecoder.decodeObject(forKey: "cellDescription") as! NSString
+            self.cellDescription = cellDescription
+            cellText = aDecoder.decodeObject(forKey: "cellText") as! NSString
+            cellImage = aDecoder.decodeObject(forKey: "cellImage") as! NSObject
+        }
+        
+        @objc func encode(with aCoder: NSCoder) {
+            aCoder.encode(cellDescription, forKey: "cellDescription")
+            aCoder.encode(cellText, forKey: "cellText")
+            aCoder.encode(cellImage, forKey: "cellImage")
+        }
+    }
+}
